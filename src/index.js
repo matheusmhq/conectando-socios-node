2c72441e6e8f85ea4a0568a4096917ae64f6a444
Matheus <mhqsoftware@gmail.com>
2021-06-27 00:29:36 -0300

    Added dynamic port

--- src/index.js
+++ src/index.js
@@ -11,4 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+var port = "";
+if (process.env.NODE_ENV === "production") process.env.PORT || 8000;
+else port = port = "3333";
+app.listen(port);
